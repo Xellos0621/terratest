b1be30c8d5d9dc9bc6f856f03af4518612a15b82
Xellos0621 <tjdwls0621@nate.com>
2023-07-14 17:59:23 +0900

    1

--- instance.tf
+++ instance.tf
@@ -1,9 +1,20 @@
+resource "tls_private_key" "mykey"{
+  algorithm = "RSA"
+  rsa_bits = 2048
+}
+
+resource "aws_key_pair" "mykey"{
+  key_name = "mykey"
+  public_key = tls_private_key.mykey.public_key_openssh
+}
+
 resource "aws_instance" "example1" {
     ami = "${lookup(var.AMIS, var.AWS_REGION)}"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.my_subnet1_pub.id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.pub_example_sg.id]
+    key_name = aws_key_pair.mykey.key_name
 
     user_data = <<-EOF
       #!/bin/bash
--- output.tf
+++ output.tf
@@ -1,6 +1,15 @@
 output "pub_ip1" {
   value = "${aws_instance.example1.public_ip}"
 }
+
 output "pri_ip2" {
   value = "${aws_instance.example2.private_ip}"
+}
+
+output "public_instance_ssh_command" {
+  value = "ssh -i mykey.pem ec2-user@${aws_instance.example1.public_ip}"
+}
+
+output "private_instance_ssh_command" {
+  value = "ssh -i mykey.pem ec2-user@${aws_instance.example2.public_ip} ssh -i mykey.pem ec2-user@${aws_instance.example2.private_ip}"
 }
