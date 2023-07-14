70c0a57e17113bbbe21533e8a5588d538412bbd4
Xellos0621 <tjdwls0621@nate.com>
2023-07-14 12:12:27 +0900

    1

--- sg.tf
+++ sg.tf
@@ -17,6 +17,12 @@ resource "aws_security_group" "pub_example_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  engress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+
   # 기타 필요한 인바운드 및 아웃바운드 규칙 설정
 }
 
@@ -37,6 +43,12 @@ resource "aws_security_group" "pri_example_sg" {
     to_port     = 0
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  engress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
   }
 
   # 기타 필요한 인바운드 및 아웃바운드 규칙 설정
